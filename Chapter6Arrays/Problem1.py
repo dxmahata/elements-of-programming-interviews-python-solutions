6455689f89138bad303c4af0a3b20e433dbf26ec
dxmahata <debanjanbhucs@gmail.com>
2015-06-04 20:14:09 -0500

    added 3rd and 4th version of the dutch national flag problem

--- Chapter6Arrays/Problem1.py
+++ Chapter6Arrays/Problem1.py
@@ -76,6 +76,65 @@ def dutchNationalFlag2(A):
                 high -= 1
         return A
     
+"""
+Separate 0s and 1s in an array: We are given an array of 0s and 1s in random order. Separate 0s on
+left side and 1s on right side of the array. Traverse array only once.
+Input = [0,1,0,1,0,0,1,1,1,0]
+Output = [0,0,0,0,0,1,1,1,1,1]
+"""
+
+def dutchNationalFlag3(A):
+    if len(A) == 0:
+        return A
+    else:
+        for elements in A:
+            assert elements in [0,1]
+        low = 0
+        high = len(A)-1
+        while low < high:
+            if A[low] == 0 and low < high:
+                low += 1
+                
+            if A[high] == 1 and low < high:
+                high -= 1
+                
+            if low < high:
+                A[low], A[high] = A[high], A[low]
+                
+        return A
+        
+
+
+
+"""
+Separate Even and Odd numbers: Given an array A, write a function that segregates even and odd
+numbers. The functions should put all even numbers first and then odd numbers.
+Example
+Input = [12,34,45,9,8,90,3]
+Output = [12,34,90,8,9,45,3]
+"""
+def dutchNationalFlag4(A):
+    if len(A) == 0:
+        return A
+    else:
+        low = 0
+        high = len(A)-1
+        
+        for elements in A:
+            assert type(elements) == type(1) or type(elements) == type(1.0)
+        
+        while (low < high):
+            if A[low]%2 == 0 and low < high:
+                low += 1
+            if A[high]%2 == 1 and low < high:
+                high -= 1
+                
+            if low < high:
+                A[low], A[high] = A[high], A[low]
+                
+        return A
+                
+
     
 if __name__ == "__main__":
     A = [4,1,6,2,29,36,100,-34]
@@ -84,4 +143,12 @@ if __name__ == "__main__":
     
     A = [0,1,1,0,1,2,1,2,0,0,0,1]
     arrangedA = dutchNationalFlag2(A)
+    print arrangedA
+    
+    A = [0,1,0,1,0,0,1,1,1,0]
+    arrangedA = dutchNationalFlag3(A)
+    print arrangedA
+    
+    A = [12,34,45,9,8,90,3]
+    arrangedA = dutchNationalFlag4(A)
     print arrangedA
