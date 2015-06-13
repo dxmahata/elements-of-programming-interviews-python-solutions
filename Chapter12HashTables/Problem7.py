162e3356e57f160ce93452fdce0ce10db76c6626
dxmahata <debanjanbhucs@gmail.com>
2015-06-13 12:31:48 -0500

    anagram partitioning

---
+++ Chapter12HashTables/Problem7.py
@@ -0,0 +1,52 @@
+'''
+Created on Jun 13, 2015
+
+@author: Debanjan Mahata
+'''
+
+"""
+Write a function that takes as input a dictionary of English words,
+and returns a partition of the dictionary into subsets of words that are all anagrams
+of each other.
+"""
+
+
+class Solution:
+    
+    def __init__(self):
+        self.englishDictList = []
+        self.anagramDict = {}
+        
+    def set_englishDictList(self,el):
+        self.englishDictList = el
+        
+    def get_englishDictList(self):
+        return self.englishDictList
+    
+    def anagram_partition(self):
+        anagram_partition_set = []
+        for entries in self.englishDictList:
+            sortedEntry = sorted(entries) #O(N*lgN)
+            sortedEntryString = "".join(sortedEntry)
+            if sortedEntryString in self.anagramDict:
+                self.anagramDict[sortedEntryString].append(entries)
+            else:
+                self.anagramDict[sortedEntryString] = [entries]
+                
+        for entries in self.anagramDict.items():
+            anagram_partition_set.append(entries[1])
+            
+        return anagram_partition_set
+             
+    
+if __name__ == "__main__":
+    
+    englishDict = ["cat", "dog", "tac", "god", "act"]
+    
+    soln = Solution()
+    
+    soln.set_englishDictList(englishDict)
+    print soln.anagram_partition()
+    
+    
+    
