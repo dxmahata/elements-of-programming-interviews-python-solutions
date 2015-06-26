8bb3060ccd92b1016847e1cd64f66ddec07cf125
dxmahata <debanjanbhucs@gmail.com>
2015-06-26 02:03:21 -0500

    Added iterative and recursive insertion sort and shell sort

--- CrackingTheCodingInterview/SortingAndSearching/InsertionSort.py
+++ CrackingTheCodingInterview/SortingAndSearching/InsertionSort.py
@@ -3,3 +3,34 @@ Created on Jun 25, 2015
 
 @author: Debanjan Mahata
 '''
+
+def insertion_sort_recursive(A, n):
+    if n == 0:
+        return 
+    else:
+        insertion_sort_recursive(A, n-1)
+        iPtr = n
+        while iPtr > 0 and A[iPtr-1] > A[iPtr]:
+            A[iPtr], A[iPtr-1] = A[iPtr-1], A[iPtr]
+            iPtr -= 1
+            
+            
+def insertion_sort_iterative(A):
+    for iPtr in range(1, len(A)):
+        jPtr = iPtr
+        currentElement = A[jPtr]
+        while jPtr > 0 and A[jPtr-1] > A[jPtr]:
+#            A[jPtr-1], A[jPtr] = A[jPtr], A[jPtr-1]
+            A[jPtr] = A[jPtr-1]
+            jPtr -= 1
+            
+        A[jPtr] = currentElement
+    return A
+            
+            
+if __name__ == "__main__":
+    A = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    insertion_sort_recursive(A, len(A)-1)
+    print A
+    B = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    print insertion_sort_iterative(B)
--- CrackingTheCodingInterview/SortingAndSearching/ShellSort.py
+++ CrackingTheCodingInterview/SortingAndSearching/ShellSort.py
@@ -3,3 +3,32 @@ Created on Jun 25, 2015
 
 @author: Debanjan Mahata
 '''
+
+
+def shell_sort(A):
+    sublistcount = len(A)//2
+    while sublistcount > 0:
+
+        for startposition in range(sublistcount):
+            shell_sort_helper(A,startposition,sublistcount)
+        
+            print("After increments of size",sublistcount,"The list is",A)
+        
+        sublistcount = sublistcount // 2
+
+
+
+def shell_sort_helper(A, start, gap):
+    for iPtr in range(start, len(A), gap):
+        jPtr = iPtr
+        currentElement = A[jPtr]
+        while jPtr > 0 and A[jPtr - gap] > A[jPtr]:
+            A[jPtr] = A[jPtr - gap]
+            jPtr -= gap
+            
+        A[jPtr] = currentElement
+        
+if __name__ == "__main__":
+    A = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    shell_sort(A)
+    
